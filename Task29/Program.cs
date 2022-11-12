e78595738ea254c8dd267713a48b1f0d855a3a3e
Kate <c.chistova2014@yandex.ru>
2022-11-12 23:30:10 +0300

    Вывод массива из N элементов

---
+++ Task29/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [size];
+
+PrintArr(array);
+Console.WriteLine();
+FillArr(array);
+PrintArr(array);
+
+void FillArr(int[] arr)
+{
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(1, 10);
+    }
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write(arr[i]);
+
+        if(i < size - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+
+    Console.Write("]");
+}
